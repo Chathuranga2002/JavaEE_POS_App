fd32ba2742a1f3f9c144d173f7b7455f2a0e21f7
Chathuranga2002 <121780050+Chathuranga2002@users.noreply.github.com>
2023-08-28 10:01:50 +0530

    update

---
+++ Back_End/src/servlet/PurchaseOrderServletAPI.java
@@ -0,0 +1,9 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {"/pages/purchase-order"})
+public class PurchaseOrderServletAPI extends HttpServlet {
+
+}
